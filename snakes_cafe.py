5f8e65156523eddb02bf667d6348bb4ce071c60f
chris turner <mr.chris.turner@gmail.com>
2018-08-14 18:08:12 -0700

    getting prices and totals
    adding up the total of the order

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -6,30 +6,52 @@ WIDTH = 48
 MENU = {
     'Appetizers':
         {
-            'Wings': 0,
-            'Spring Rolls': 0,
-            'Rolls': 0,
+            'Wings': [0, 3.99],
+            'Spring Rolls': [0, 7.99],
+            'Rolls': [0, 2.99],
+            'Cookies': [0, 4.99],
+            'French Fires': [0, 3.99],
+            'Poutine': [0, 7.99],
         },
-        
+
     'Entrees':
         {
-            'Hamburger': 0,
-            'Lobster': 0,
-            'Meatball Sub': 0,
+            'Hamburger': [0, 12.99],
+            'Lobster': [0, 17.99],
+            'Meatball Sub': [0, 9.99],
+            'Pizza': [0, 14.99],
+            'Hot Pocket': [0, 7.99],
+            'PB&J': [0, 5.99],
         },
 
     'Desserts':
         {
-            'Cake': 0,
-            'Ice Cream': 0,
-            'Pie': 0,
+            'Cake': [0, 2.99],
+            'Ice Cream': [0, 1.99],
+            'Pie': [0, 4.99],
+            'Malt': [0, 3.99],
+            'Rootbeer Float': [0, 4.99],
+            'Shake': [0, 3.99],
         },
-        
+
     'Drinks':
         {
-            'Tea': 0,
-            'Coffee': 0,
-            'Soda': 0,
+            'Tea': [0, .99],
+            'Coffee': [0, 1.99],
+            'Soda': [0, 1.99],
+            'Beer': [0, 6.99],
+            'Hot Tea': [0, .99],
+            'Milk': [0, .99],
+        },
+
+    'Sides':
+        {
+            'Salad': [0, 2.99],
+            'Beans': [0, 1.99],
+            'Steamed Vegetables': [0, 3.99],
+            'Coleslaw': [0, 2.99],
+            'Baked Potato': [0, 5.99],
+            'Eggs': [0, 2.99],
         },
 }
 
@@ -58,11 +80,21 @@ def display_menu():
     ln_one = 'Here are the menu items'
     print(ln_one)
 
-    for food_type, items in MENU.items():
+    for food_type, dishes in MENU.items():
         print(dedent(food_type))
-        
-        for key in items:
-            print(dedent(key))
+
+        for dish in dishes:
+            print(dedent(dish), dishes[dish][1])
+
+
+def total_order():
+    total_price = 0
+    for food_type, dishes in MENU.items():
+        for dish in dishes:
+            if dishes[dish][0] > 0:
+                total_price += (dishes[dish][0] * dishes[dish][1])
+    print(total_price)
+
 
 
 def process_input(user_input):
@@ -75,12 +107,14 @@ def process_input(user_input):
     for food_type, dishes in MENU.items():
         for dish in dishes:
             if user_input.title() == dish:
-                dishes[user_input.title()] += 1
+                # print(dishes[str(dish)][0])
+                dishes[dish][0] += 1
                 item = dishes[user_input.title()]
-                ordered = True   
+                ordered = True
 
     if ordered:
-        print('You have ordered', str(item), user_input.title())
+        print('You have ordered', str(item)[1], user_input.title())
+        print(total_order())
     else:
         print('Please try to order something on the menu!')
 
@@ -96,6 +130,7 @@ def order():
     '''))
     process_input(user_input)
 
+
 def exit():
     print(dedent('''
         'Thanks for visiting'
@@ -106,7 +141,7 @@ def run():
     greeting()
     display_menu()
     order()
-    
+
 
 if __name__ == '__main__':
     run()
