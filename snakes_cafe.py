4abfe2eb5fd5a6b890779466297db5569c9121d9
chris turner <mr.chris.turner@gmail.com>
2018-08-14 18:43:14 -0700

    2 digit totals

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,4 +1,6 @@
 from textwrap import dedent
+from math import pi
+
 import sys
 
 WIDTH = 48
@@ -6,52 +8,52 @@ WIDTH = 48
 MENU = {
     'Appetizers':
         {
-            'Wings': [0, 3.99],
-            'Spring Rolls': [0, 7.99],
-            'Rolls': [0, 2.99],
-            'Cookies': [0, 4.99],
-            'French Fires': [0, 3.99],
-            'Poutine': [0, 7.99],
+            'Wings': [0, 3.44],
+            'Spring Rolls': [0, 7.55],
+            'Rolls': [0, 2.66],
+            'Cookies': [0, 4.34],
+            'French Fires': [0, 3.21],
+            'Poutine': [0, 7.65],
         },
 
     'Entrees':
         {
-            'Hamburger': [0, 12.99],
-            'Lobster': [0, 17.99],
-            'Meatball Sub': [0, 9.99],
-            'Pizza': [0, 14.99],
+            'Hamburger': [0, 12.79],
+            'Lobster': [0, 17.49],
+            'Meatball Sub': [0, 9.85],
+            'Pizza': [0, 14.26],
             'Hot Pocket': [0, 7.99],
-            'PB&J': [0, 5.99],
+            'Pasta': [0, 5.99],
         },
 
     'Desserts':
         {
-            'Cake': [0, 2.99],
-            'Ice Cream': [0, 1.99],
-            'Pie': [0, 4.99],
-            'Malt': [0, 3.99],
-            'Rootbeer Float': [0, 4.99],
+            'Cake': [0, 2.00],
+            'Ice Cream': [0, 1.04],
+            'Pie': [0, 4.08],
+            'Malt': [0, 3.79],
+            'Rootbeer Float': [0, 4.59],
             'Shake': [0, 3.99],
         },
 
     'Drinks':
         {
-            'Tea': [0, .99],
+            'Tea': [0, .79],
             'Coffee': [0, 1.99],
             'Soda': [0, 1.99],
-            'Beer': [0, 6.99],
+            'Beer': [0, 6.49],
             'Hot Tea': [0, .99],
             'Milk': [0, .99],
         },
 
     'Sides':
         {
-            'Salad': [0, 2.99],
-            'Beans': [0, 1.99],
-            'Steamed Vegetables': [0, 3.99],
-            'Coleslaw': [0, 2.99],
+            'Salad': [0, 2.39],
+            'Beans': [0, 1.96],
+            'Steamed Vegetables': [0, 3.49],
+            'Coleslaw': [0, 2.79],
             'Baked Potato': [0, 5.99],
-            'Eggs': [0, 2.99],
+            'Eggs': [0, 2.19],
         },
 }
 
@@ -93,6 +95,8 @@ def total_order():
         for dish in dishes:
             if dishes[dish][0] > 0:
                 total_price += (dishes[dish][0] * dishes[dish][1])
+
+    total_price = '{0:.2f}'.format(total_price)
     print(total_price)
 
 
